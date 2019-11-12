2ba0b14bd9cb01d8f18c7f76e064d36e54624162
Eduardo Pinheiro <eduardo.pinheiro@jussi.local>
2019-11-12 16:26:38 -0300

    fix bugs & login

--- app/src/main/java/com/example/desafiomarvel/data/CadastroDao.java
+++ app/src/main/java/com/example/desafiomarvel/data/CadastroDao.java
@@ -6,13 +6,23 @@ import androidx.room.Query;
 
 import com.example.desafiomarvel.model.Cadastro;
 
+import io.reactivex.Observable;
+
 @Dao
 public interface CadastroDao {
 
     @Insert
     void insereCadastro(Cadastro cadastro);
 
+    @Query("SELECT * FROM cadastros")
+    Observable<Cadastro> pegaCadastro();
+
     @Query("SELECT * FROM cadastros WHERE email = :emailUser")
     Cadastro getByEmail(String emailUser);
+
+    @Query("SELECT * FROM cadastros WHERE senha = :senhaUser")
+    Cadastro getBySenha(String senhaUser);
+
+
 
 }
--- app/src/main/java/com/example/desafiomarvel/view/activity/LoginActivity.java
+++ app/src/main/java/com/example/desafiomarvel/view/activity/LoginActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,6 +14,9 @@ import com.example.desafiomarvel.data.CadastroDao;
 import com.example.desafiomarvel.data.Database;
 import com.example.desafiomarvel.model.Cadastro;
 import com.google.android.material.textfield.TextInputLayout;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -29,37 +33,29 @@ public class LoginActivity extends AppCompatActivity {
         initViews();
 
 
-//            btnLogin.setOnClickListener(view->{
-//                new Thread(()->{
-//                    if(!inputEmail.getEditText().getText().toString().isEmpty() &&
-//                            !inputSenha.getEditText().getText().toString().isEmpty()){
-//
-//                        String email = inputEmail.getEditText().getText().toString();
-//                        cadastroDao.getByEmail(email);
-//
-//                        Cadastro cadastro = new Cadastro("u", email, "s");
-//
-//                        if(email == cadastro.getEmailUser()) {
-//
-//                            Intent intent = new Intent(this, HomeActivity.class);
-//                            startActivity(intent);
-//                        }
-//
-//                    }else{
-//                        Toast.makeText(getApplicationContext(), "Preencha todos os campos!", Toast.LENGTH_SHORT).show();
-//                    }
-//                }).start();
-//            });
+            btnLogin.setOnClickListener(view->{
+                new Thread(()->{
+                    if(!inputEmail.getEditText().getText().toString().isEmpty() &&
+                        !inputSenha.getEditText().getText().toString().isEmpty()){
+                            pegaCadastro();
+                    }else{
+                        Toast.makeText(getApplicationContext(),
+                                "Preencha todos os campos!",
+                                    Toast.LENGTH_SHORT).show();
+                    }
 
-        btnLogin.setOnClickListener(view->{
-            if(!inputEmail.getEditText().getText().toString().isEmpty() &&
-                    !inputSenha.getEditText().getText().toString().isEmpty()){
-                Intent intent = new Intent(this, HomeActivity.class);
-                startActivity(intent);
-            }else{
-                Toast.makeText(getApplicationContext(), "Preencha todos os campos!", Toast.LENGTH_SHORT).show();
-            }
-        });
+                }).start();
+            });
+
+//        btnLogin.setOnClickListener(view->{
+//            if(!inputEmail.getEditText().getText().toString().isEmpty() &&
+//                    !inputSenha.getEditText().getText().toString().isEmpty()){
+//                Intent intent = new Intent(this, HomeActivity.class);
+//                startActivity(intent);
+//            }else{
+//                Toast.makeText(getApplicationContext(), "Preencha todos os campos!", Toast.LENGTH_SHORT).show();
+//            }
+//        });
 
         btnRegistro.setOnClickListener(view ->{
             Intent intent = new Intent(this, RegistroActivity.class);
@@ -72,8 +68,22 @@ public class LoginActivity extends AppCompatActivity {
         inputSenha = findViewById(R.id.inputSenhaRegistro);
         btnLogin = findViewById(R.id.buttonLogin);
         btnRegistro = findViewById(R.id.loginButtonCadastrese);
-
         cadastroDao = Database.getDatabase(this).cadastroDao();
+    }
+
+    private void pegaCadastro(){
+        cadastroDao.pegaCadastro()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(cadastro -> {
+                    if(cadastro.getEmailUser() == inputEmail.getEditText().getText().toString() &&
+                    cadastro.getSenhaUser() == inputSenha.getEditText().getText().toString()){
+                        Intent intent = new Intent(this, HomeActivity.class);
+                        startActivity(intent);
+                    }
+                }, throwable -> {
+                    Log.i("TAG", "erro" + throwable.getMessage());
+                });
     }
 
 }
